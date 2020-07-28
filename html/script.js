20f7dd2495112b2bb32a3d12dd437874b88b9a67
Honda-Takumi <i52334@ga.ariake-nct.ac.jp>
2020-07-28 16:02:00 +0900

    Updata index.html & create script.js
    長押しで反応するボタンの作成完了
    
    JSはクラスがゴミなのでTypeScriptに移行します

---
+++ html/script.js
@@ -0,0 +1,94 @@
+var serial;
+
+
+function init() {
+    serial = new Serial("usb1")
+}
+
+function fwrd() {
+    //serial.write("F"),
+    console.log('F')
+}
+
+function left() {
+    //serial.write("L"),
+    console.log('L')
+}
+
+
+function rigt() {
+    //serial.write("R"),
+    console.log('R')
+}
+
+function back() {
+    //serial.write("B"),
+    console.log('B')
+}
+
+function liftup() {
+    //serial.write("LU"),
+    console.log('LU')
+}
+
+function liftdown() {
+    //serial.write("LD"),
+    console.log('LD')
+}
+
+window.onload = function () { // ページ読み込み時に実行
+
+    //ここから先どうにかしてまとめて
+    document.getElementById("fwrd").onmousedown = function () {
+        $intervalID = setInterval(
+            function () {
+                fwrd();
+            },
+            100
+        );
+    }
+
+    document.getElementById("fwrd").onmouseup = function () {
+        clearInterval($intervalID);
+    }
+
+
+    document.getElementById("back").onmousedown = function () {
+        $intervalID = setInterval(
+            function () {
+                back();
+            },
+            100
+        );
+    }
+
+    document.getElementById("back").onmouseup = function () {
+        clearInterval($intervalID);
+    }
+
+    document.getElementById("rigt").onmousedown = function () {
+        $intervalID = setInterval(
+            function () {
+                rigt();
+            },
+            100
+        );
+    }
+    document.getElementById("rigt").onmouseup = function () {
+        clearInterval($intervalID);
+    }
+
+    document.getElementById("left").onmousedown = function () {
+        $intervalID = setInterval(
+            function () {
+                left();
+            },
+            100
+        );
+    }
+
+    document.getElementById("left").onmouseup = function () {
+        clearInterval($intervalID);
+    }
+
+}
