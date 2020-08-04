6d424c37d8acce55dd00545e78cb0a5406e19c2f
Minamitsu6 <39166057+Honda-Takumi@users.noreply.github.com>
2020-08-04 13:12:57 +0900

    reacticeXを使わないコードにしろ

--- html/control.js
+++ html/control.js
@@ -1,33 +1,76 @@
-var Sendchar = /** @class */ (function () {
-    function Sendchar() {
+"use strict";
+exports.__esModule = true;
+//npm install browserify vinyl-source-stream rx --save
+require("./style.css");
+var fromEvent_1 = require("rxjs/observable/fromEvent");
+//export var serial: any;
+var Move = /** @class */ (function () {
+    function Move() {
         var message = "Hello! TypeScript!";
         console.log(message);
-        //var serial = new Serial("usb1");
     }
-    Sendchar.prototype.fwrd = function () {
-        //serial.write("F"),
+    Move.prototype.fwrd = function () {
+        //serial.write("F");
         console.log("F");
     };
-    Sendchar.prototype.left = function () {
-        //serial.write("L"),
+    Move.prototype.left = function () {
+        //serial.write("L");
         console.log("L");
     };
-    Sendchar.prototype.rigt = function () {
-        //serial.write("R"),
+    Move.prototype.rigt = function () {
+        //serial.write("R");
         console.log("R");
     };
-    Sendchar.prototype.back = function () {
-        //serial.write("B"),
+    Move.prototype.back = function () {
+        //serial.write("B");
         console.log("B");
     };
-    Sendchar.prototype.liftup = function () {
-        //serial.write("LU"),
+    Move.prototype.liftup = function () {
+        //serial.write("LU");
         console.log("LU");
     };
-    Sendchar.prototype.liftdown = function () {
-        //serial.write("LD"),
+    Move.prototype.liftdown = function () {
+        //serial.write("LD");
         console.log("LD");
     };
-    return Sendchar;
+    return Move;
 }());
-var s = new Sendchar();
+var Buttontap = /** @class */ (function () {
+    function Buttontap(element) {
+        console.log("test");
+        this._el = element;
+        /*window.addEventListener("onmousedown", (event) => {
+          console.log("push");
+        });*/
+        this.detect();
+    }
+    Buttontap.prototype.detect = function () {
+        var _this = this;
+        var mousedown$ = fromEvent_1.fromEvent(this._el, "mousedown");
+        var mouseup$ = fromEvent_1.fromEvent(this._el, "mouseup");
+        mousedown$.pipe((this.intervalId = window.setInterval(function () {
+            var M = new Move();
+            switch (_this._el) {
+                case "fwrd":
+                    M.fwrd();
+                    break;
+                case "back":
+                    M.back();
+                    break;
+                case "left":
+                    M.left();
+                    break;
+                case "rigt":
+                    M.rigt();
+                    break;
+                case "liftdown":
+                    M.liftdown();
+                case "liftup":
+                    M.liftup();
+                    break;
+            }
+        }, 100)));
+        mouseup$.pipe(window.clearInterval(this.intervalId));
+    };
+    return Buttontap;
+}());
--- html/control.ts
+++ html/control.ts
@@ -1,39 +1,85 @@
-class Sendchar {
+//npm install browserify vinyl-source-stream rx --save
+import "./style.css";
+import { Observable } from "Observable";
+import { fromEvent } from "observable/fromEvent";
+import { tap, delay } from "operators";
+
+//export var serial: any;
+class Move {
   constructor() {
     const message: string = "Hello! TypeScript!";
     console.log(message);
-    //var serial = new Serial("usb1");
   }
   fwrd(): void {
-    //serial.write("F"),
+    //serial.write("F");
     console.log("F");
   }
 
   left(): void {
-    //serial.write("L"),
+    //serial.write("L");
     console.log("L");
   }
 
   rigt(): void {
-    //serial.write("R"),
+    //serial.write("R");
     console.log("R");
   }
 
   back(): void {
-    //serial.write("B"),
+    //serial.write("B");
     console.log("B");
   }
 
   liftup(): void {
-    //serial.write("LU"),
+    //serial.write("LU");
     console.log("LU");
   }
 
   liftdown(): void {
-    //serial.write("LD"),
+    //serial.write("LD");
     console.log("LD");
   }
 }
+class Buttontap {
+  _el: any;
+  intervalId: number;
 
-var s: Sendchar = new Sendchar();
-//http://www.shurey.com/js/samples/1_tips16.html
+  constructor(element) {
+    console.log("test");
+    this._el = element;
+    /*window.addEventListener("onmousedown", (event) => {
+      console.log("push");
+    });*/
+    this.detect();
+  }
+  detect() {
+    const mousedown$: Observable<Event> = fromEvent(this._el, "mousedown");
+    const mouseup$: Observable<Event> = fromEvent(this._el, "mouseup");
+    mousedown$.pipe(
+      (this.intervalId = window.setInterval(() => {
+        var M: Move = new Move();
+        switch (this._el) {
+          case "fwrd":
+            M.fwrd();
+            break;
+          case "back":
+            M.back();
+            break;
+          case "left":
+            M.left();
+            break;
+          case "rigt":
+            M.rigt();
+            break;
+          case "liftdown":
+            M.liftdown();
+          case "liftup":
+            M.liftup();
+            break;
+        }
+      }, 100))
+    );
+
+    mouseup$.pipe(window.clearInterval(this.intervalId));
+  }
+}
